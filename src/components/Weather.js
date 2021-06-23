d493a2f66cd1034cc67a6f2cfb4d082e7d8283f4
Omar Quazi <omar@quazi.co>
2021-06-23 02:24:43 -0400

    persist weather data in storage so that we dont make too many API calls

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import ReactBootstrap, {Button} from 'react-bootstrap';
+import ReactBootstrap, { Container, Col, Row, Alert, Button } from 'react-bootstrap';
 
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '99b84d8baf9a2dbccdcf348d52979b99';
+const WEATHER_APP_STORAGE = 'weatherApp.Storage';
 
 export default function Weather() {
 
     const [location, setLocation] = useState({});
+
+    useEffect(() => {
+        setLocation(JSON.parse(localStorage.getItem(WEATHER_APP_STORAGE) || '{}'));
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(WEATHER_APP_STORAGE, JSON.stringify(location));
+    }, [location])
 
     async function fetchWeather(url) {
         return fetch(url)
@@ -26,13 +35,13 @@ export default function Weather() {
                         lat: position.coords.latitude,
                         long: position.coords.longitude
                     };
-                    url += `?lat=${coords.lat}&lon=${coords.long}&appid=${API_KEY}`;
-                    fetchWeather(url).then(data => console.log(data));
+                    url += `?lat=${coords.lat}&lon=${coords.long}&units=metric&appid=${API_KEY}`;
+                    fetchWeather(url).then(data => setLocation(data));
                 },
                 function (error) {
                     console.error("Error Code = " + error.code + " - " + error.message);
-                    url += `?q=Toronto&appid=${API_KEY}`;
-                    fetchWeather(url).then(data => console.log(data));
+                    url += `?q=Toronto&units=metric&appid=${API_KEY}`;
+                    fetchWeather(url).then(data => setLocation(data));                
                 },
                 {
                     enableHighAccuracy: true,
@@ -42,15 +51,24 @@ export default function Weather() {
             );
         } else {
             console.log("Cannot access navigator");
-            url += `?q=Toronto&appid=${API_KEY}`;
-            fetchWeather(url).then(data => console.log(data));        
+            url += `?q=Toronto&units=metric&appid=${API_KEY}`;
+            fetchWeather(url).then(data => setLocation(data));
         }
         
     }
 
     return (
-        <div>
-            <Button variant="primary" onClick={getWeather}>Get Weather</Button>
-        </div>
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                    <Alert variant="primary">
+                        The location name is {location.name}
+                    </Alert>
+                    <Button variant="primary" onClick={getWeather}>Get Weather</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
