c813251476bbe6e7bb9eba2a69cef7c6619468ef
Omar Quazi <omar@quazi.co>
2021-06-24 04:23:58 -0400

    remove dead code

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import './App.css';
 import Header from './components/Header';
 import Weather from './components/Weather';
 import Footer from './components/Footer';
-// import FirstTime from './components/FirstTime';
-import React, { useState, useEffect } from 'react';
-
-const WEATHER_STORAGE_KEY = 'weatherApp.Storage';
+import React from 'react';
 
 function App() {
-
-  const [showFirstTime, setShowFirstTime] = useState(false);
-
-  useEffect(() => {
-    setShowFirstTime(JSON.parse(localStorage.getItem(WEATHER_STORAGE_KEY + ".showFirstTime") || false));
-  }, [])
-
-  useEffect(() => {
-      localStorage.setItem(WEATHER_STORAGE_KEY + ".showFirstTime", showFirstTime);
-  }, [showFirstTime])
-
   return (
     <>
-      
-      {showFirstTime === false && <Header />}
-      {showFirstTime === false && <Weather />}
-      {!showFirstTime && <Footer />}
-      {/* {showFirstTime === true && <FirstTime onClick={(v) => setShowFirstTime(false)} />} */}
+      <Header />
+      <Weather />
+      <Footer />
     </>
   );
-
-
 }
 
 export default App;
